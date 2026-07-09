2538f61ca124c642641d799f89f6b1d97361ce83
Amar Kulovac <kulovac@ualberta.ca>
2026-07-09 18:35:57 -0400

    Set internal clock to max frequencies (for now)

--- firmware/src/bsp.c
+++ firmware/src/bsp.c
@@ -1,8 +1,18 @@
 #include "bsp.h"
+#include "stm32f4xx.h"
 #include "stm32f4xx_ll_bus.h"
 #include "stm32f4xx_ll_gpio.h"
+#include "stm32f4xx_ll_rcc.h"
+#include "stm32f4xx_ll_utils.h"
+#include "system_stm32f4xx.h"
+
+#define CLOCK_FREQ 100000000UL
+
+static void clock_init(void);
 
 void bsp_init(void) {
+    clock_init();
+
     LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
 
     LL_GPIO_InitTypeDef led = {.Pin = STATUS_LED_PIN,
@@ -12,4 +22,38 @@ void bsp_init(void) {
                                .Pull = LL_GPIO_PULL_NO,
                                .Alternate = LL_GPIO_AF_0};
     LL_GPIO_Init(STATUS_LED_PORT, &led);
+}
+
+static void clock_init(void) {
+    // NOTE: All values are set to their max
+    // according to the datasheet
+    //
+    // AHB(max) = 100MHz
+    // APB1(max) = 50Mhz
+    // APB2(max) = 100Mhz
+
+    // TODO: lower clock frequency as much as
+    // possible and determine power savings
+
+    LL_UTILS_PLLInitTypeDef pll_init = {
+        .PLLM = LL_RCC_PLLM_DIV_8, // 16MHz / 8 = 2MHz
+        .PLLN = 100,               // 2MHz * 100 = 200MHz
+        .PLLP = LL_RCC_PLLP_DIV_2  // 200MHz / 2 = 100MHz
+    };
+
+    LL_UTILS_ClkInitTypeDef clk_init = {
+        .AHBCLKDivider = LL_RCC_SYSCLK_DIV_1, // 100MHz
+        .APB1CLKDivider = LL_RCC_APB1_DIV_2,  // 50MHz
+        .APB2CLKDivider = LL_RCC_APB2_DIV_1,  // 100MHz
+    };
+
+    ErrorStatus e = LL_PLL_ConfigSystemClock_HSI(&pll_init, &clk_init);
+    if (e == ERROR) {
+        // TODO: error handling, but you'll know if nothing turns on...
+        for (;;) {
+        }
+    }
+
+    SystemCoreClock = CLOCK_FREQ;
+    LL_Init1msTick(SystemCoreClock);
 }
