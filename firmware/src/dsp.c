825d14d7e9264dd0ab07aa7ab179ab041f42903c
Amar Kulovac <kulovac@ualberta.ca>
2026-07-11 13:33:47 -0400

    Created stub for running yin algorithm

---
+++ firmware/src/dsp.c
@@ -0,0 +1,7 @@
+#include "dsp.h"
+
+float compute_yin(float sig[BUFFER_SIZE], int32_t sr, size_t w_len,
+                  size_t w_step, int32_t f0_min, int32_t f0_max,
+                  float harmo_thresh) {
+    return 0.0f;
+}
